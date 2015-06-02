250eb0053bb89335022e666700d249bfd926a7cf
revoicelinelice <alinelice@naver.com>
2015-06-03 01:15:20 +0900

    robot1 dance save
    robot1 dance save

--- src/RobotDance.java
+++ src/RobotDance.java
@@ -12,10 +12,16 @@ public class RobotDance extends Dance {
 		.getAnimator()
 		.start()
 		.parallel(0.5, 
-					new MovementTransition(target.headBranch).setX(0.4),
-					 new MovementTransition(target.headBranch).setX(-0.4)
+					new MovementTransition(target.headBranch).setX(0.1).setY(0.1),
+					 new MovementTransition(target.headBranch).setX(-0.1).setY(0.1),
+					 new MovementTransition(target.leftArmBranch).setX(0.7).setY(-0.3),
+					 new MovementRotation(target.leftArmBranch).setZ(90),
+					 new MovementTransition(target.rightArmBranch).setX(-0.7).setY(-0.3),
+					 new MovementRotation(target.rightArmBranch).setZ(-90),
+					 new MovementRotation(target.bodyBranch).setX(20),
+					 new MovementRotation(target.bodyBranch).setX(-20)
 				)
-		.repeat(100)
+		.repeat(4)
 		.end()
 		.commit();
 	}
