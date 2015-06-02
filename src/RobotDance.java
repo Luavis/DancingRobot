8f4670a83a4be59db10ebd36339d7f3600d3f0c7
revoicelinelice <alinelice@naver.com>
2015-06-02 11:58:56 +0900

    Commit

--- src/RobotDance.java
+++ src/RobotDance.java
@@ -10,19 +10,18 @@ public class RobotDance extends Dance {
 	public void initDance() {
 		target
 		.getAnimator()
-		.parallel(3, new MovementRotation(target.branch).setY(360))
-		.repeat(10)
-		.parallel(1, new MovementRotation(target.leftArmBranch).setZ(90).setDeltaSecond(1),
-					 new MovementTransition(target.leftArmBranch).setX(0.7).setY(-0.3).setDeltaSecond(1),
-					 new MovementRotation(target.rightArmBranch).setZ(-90).setDeltaSecond(1),
-					 new MovementTransition(target.rightArmBranch).setX(-0.7).setY(-0.3).setDeltaSecond(1)
+//		.parallel(0.5, new MovementRotation(target.leftArmBranch).setZ(90).setDeltaSecond(0.5),
+//					 new MovementTransition(target.leftArmBranch).setX(0.7).setY(-0.3).setDeltaSecond(0.5),
+//					 new MovementRotation(target.rightArmBranch).setZ(-90).setDeltaSecond(0.5),
+//					 new MovementTransition(target.rightArmBranch).setX(-0.7).setY(-0.3).setDeltaSecond(0.5)
+//				)
+//		.repeat(2)
+		.parallel(0.3, new MovementTransition(target.headBranch).setY(0.1).setDeltaSecond(0.3)
+				)
+		.parallel(0.2, new MovementTransition(target.headBranch).setX(0.3).setDeltaSecond(0.2)
 				)
 		.repeat(1)
-		.parallel(1, new MovementTransition(target.headBranch).setX(0.1).setY(0.1).setDeltaSecond(0.3),
-				new MovementTransition(target.headBranch).setX(-0.1).setY(-0.1).setDeltaSecond(0.3),
-				new MovementTransition(target.headBranch).setX(-0.1).setY(0.1).setDeltaSecond(0.3),
-				new MovementTransition(target.headBranch).setX(0.1).setY(-0.1).setDeltaSecond(0.3)
-				)
+		.repeat(1)
 		.commit();
 		
 	}
