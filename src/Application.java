17e6e597b945f3a02034e4d834b79df57b01053e
Luavis <luaviskang@gmail.com>
2015-06-07 23:13:14 +0900

    make compatible with windows

--- src/Application.java
+++ src/Application.java
@@ -39,14 +39,31 @@ public class Application {
 		this.manager.start();
 	}
 	
-	@SuppressWarnings("restriction")
 	public void enableOSXFullscreen() {
 		try {
-			com.apple.eawt.FullScreenUtilities.setWindowCanFullScreen(this.windowFrame, true);
-			com.apple.eawt.Application.getApplication().requestToggleFullScreen(this.windowFrame);
+			/* 
+			 * origin format for compatibility with windows
+			 *	 com.apple.eawt.FullScreenUtilities.setWindowCanFullScreen(this.windowFrame, true);
+			 *	 com.apple.eawt.Application.getApplication().requestToggleFullScreen(this.windowFrame);
+			 */
+			
+			Class<?> fullscreenUtiles = Class.forName("com.apple.eawt.FullScreenUtilities");
+			Class<?>[] fullscreenUtilsArgTypes = new Class[] { Window.class, boolean.class };
+			Method setWindowCanFullScreen = fullscreenUtiles.getMethod("setWindowCanFullScreen", fullscreenUtilsArgTypes);
+			setWindowCanFullScreen.invoke(fullscreenUtiles, this.windowFrame, true);
+			
+			Class<?> application = Class.forName("com.apple.eawt.Application");
+			Class<?>[] applicationArgTypes = new Class[] { };
+			Method getApplication = application.getMethod("getApplication", applicationArgTypes);
+			Object applicationObject = getApplication.invoke(application);
+			
+			Class<?>[] requestToggleFullScreenArgTypes = new Class[] {Window.class };
+			Method requestToggleFullScreen = application.getMethod("requestToggleFullScreen", requestToggleFullScreenArgTypes);
+			requestToggleFullScreen.invoke(applicationObject, this.windowFrame);
 	    }
 	    catch (Exception e) {
 	    	System.out.println("OS X full screen not support or Not OS X");
+	    	e.printStackTrace();
 	    }
 	}
 }
